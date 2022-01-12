940a93cb8f3cd9b15094b4d1650495f3e3c4607a
dario <dariopizzi12@gmail.com>
2022-01-12 19:56:46 -0300

    cambios

--- contacto.php
+++ contacto.php
@@ -17,33 +17,10 @@
 </head>
 
 <body id="contacto">
-    <nav class="navbar navbar-expand-md pt-0 mt-1">
-        <div class="container">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="fas fa-bars"></span>
-            </button>
-            <div class="collapse navbar-collapse " id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0 mt-2">
-                    <li class="nav-item px-1">
-                        <a class="nav-link px-4 py-1" href="index.php">Inicio</a>
-                    </li>
-                    <li class="nav-item px-1">
-                        <a class="nav-link px-4 py-1" href="sobre-mi.php">Sobre mí</a>
-                    </li>
-                    <li class="nav-item px-1">
-                        <a class="nav-link px-4 py-1" href="proyectos.php">Proyectos</a>
-                    </li>
-                    <li class="nav-item px-1">
-                        <a class="nav-link active px-4 py-1" href="contacto.php">Contacto</a>
-                    </li>
-                </ul>
-                <div class="d-inline">
-                    <a href="" class="btn btn-rojo">Descargar mi CV <i class="fas fa-download"></i></a>
-                </div>
-            </div>
-        </div>
-    </nav>
+
+<?php include_once "header.php" ?>
+
+
     <main class="container">
         <div class="row">
             <div class="col-12 mt-3 mb-5">
