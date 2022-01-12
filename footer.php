9a99c094c9b1c9686434ea461bafeb541c2987c6
dario <dariopizzi12@gmail.com>
2022-01-12 20:53:23 -0300

    cambios

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer class="container mb-0">
+            <div class="row">
+                <div class="col-12 col-sm-3"> <a href="https://github.com" target="_blank" title="Github"><i
+                            class="fab fa-github px-2"></i></a>
+                    <a href="https://www.linkedin.com/in/nelson-daniel-tarche/" target="_blank" title="Linkedin"><i
+                            class="fab fa-linkedin-in px-2 "></i></a>
+                </div>
+                <div class="col-12 col-sm-3"> Sponsor <a href="http://" target="_blank" rel="noopener noreferrer">DePC Suite</a>
+                </div>
+                <div class="col-12 col-sm-3">dariopizzi12@gmail.com</div>
+            </div>
+            <div class="bloquewhatsapp pb-4 pt-3 px-3">
+                <a href=""><i class="fab fa-whatsapp"></i></a>
+            </div>
+
+        </footer>
